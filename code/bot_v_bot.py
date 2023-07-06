ff789c1e4568bd3674b7b04842e795ead6e47e5d
chenlm <chenlingmin.com@gmail.com>
2023-07-06 17:49:00 +0800

    美化棋盘

--- code/bot_v_bot.py
+++ code/bot_v_bot.py
@@ -3,7 +3,7 @@ import time
 from dlgo import goboard_slow as goboard
 from dlgo import gotypes
 from dlgo.agent.naive import RandomBot
-from dlgo.utils import print_board, print_move
+from dlgo.utils import print_board, print_move, print_board_plus
 
 
 def main():
@@ -17,7 +17,8 @@ def main():
         time.sleep(0.3)
 
         print(chr(27) + "[2J")
-        print_board(game.board)
+        print_board_plus(game.board)
+        # print_board(game.board)
         bot_move = bots[game.next_player].select_move(game)
         print_move(game.next_player, bot_move)
         game = game.apply_move(bot_move)
--- code/dlgo/utils.py
+++ code/dlgo/utils.py
@@ -27,3 +27,43 @@ def print_board(board):
             line.append(STONE_TO_CHAR[stone])
         print("%s%d %s" % (bump, row, ''.join(line)))
     print('    ' + '  '.join(COLS[:board.num_cols]))
+
+
+def print_board_plus(board):
+    """显示局面"""
+
+    num_rows = board.num_rows
+    num_cols = board.num_cols
+
+    for row in range(num_rows, 0, -1):
+        bump = " " if row <= 9 else ""
+        for col in range(1, board.num_cols + 1):
+            stone = board.get(gotypes.Point(row=row, col=col))
+            if stone == gotypes.Player.black:
+                chessman = ' ● '
+            elif stone == gotypes.Player.white:
+                chessman = ' ୦ '
+            else:
+                if row == num_rows:
+                    if col == num_cols:
+                        chessman = '─┐ '
+                    elif col == 1:
+                        chessman = ' ┌─'
+                    else:
+                        chessman = '─┬─'
+                elif row == 1:
+                    if col == num_cols:
+                        chessman = '─┘ '
+                    elif col == 1:
+                        chessman = ' └─'
+                    else:
+                        chessman = '─┴─'
+                elif col == num_cols:
+                    chessman = '─┤ '
+                elif col == 1:
+                    chessman = ' ├─'
+                else:
+                    chessman = '─┼─'
+            print('\033[0;30;43m' + chessman + '\033[0m', end='')
+        print("%s%d" % (bump, row))
+    print(' ' + '  '.join(COLS[:board.num_cols]))
